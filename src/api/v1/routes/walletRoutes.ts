3cceff2a7942e0d517e072d75aa81b221e9c8084
Kenneth Jimmy <kenjimmy17@gmail.com>
2022-06-13 21:16:37 +0100

    ✅ Get user transations route

--- src/api/v1/routes/walletRoutes.ts
+++ src/api/v1/routes/walletRoutes.ts
@@ -17,11 +17,12 @@ router.use(catchAsync(protect));
 router
   .route('/fund-my-wallet')
   .patch(topUp, validationResults, catchAsync(controller.fundMyWallet));
-
 router.route('/find-user/:walletId').get(catchAsync(controller.findWalletUser));
 router
   .route('/transfer-fund')
   .post(transfer, validationResults, catchAsync(controller.transferFund));
+
+router.route('/my-transactions').get(catchAsync(controller.getMyTransactions));
 
 // Typical Admin routes
 router.route('/').get(catchAsync(controller.getAllWallets));
